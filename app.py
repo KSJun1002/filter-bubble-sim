7bd961729071373523d5043f0f2635e7f6f9249f
KSJun1002 <ksjun1002naver.com@gmail.com>
2026-06-10 18:57:58 +0900

    Add matplotlib configuration for Agg backend

--- app.py
+++ app.py
@@ -7,6 +7,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg') 
+
 import matplotlib.pyplot as plt
 
 # ------------------------------------------------------------------------------
